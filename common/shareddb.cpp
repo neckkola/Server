6b9456448e504ca6385181190e1e5a1bd4aa694b
Mitch Freeman <65987027+neckkola@users.noreply.github.com>
2024-09-22 11:34:18 -0300

    WIP for evolve xp transfer

--- common/shareddb.cpp
+++ common/shareddb.cpp
@@ -822,7 +822,7 @@ bool SharedDatabase::GetInventory(Client *c)
 				inst->SetEvolveActivated(e.activated);
 				inst->SetEvolveEquiped(e.equiped);
 				inst->SetEvolveCurrentAmount(e.current_amount);
-				inst->SetEvolveProgression(e.progression);
+				inst->SetEvolveProgression2();
 				inst->SetEvolveFinalItemID(e.final_item_id);
 			}
 			else {
@@ -832,7 +832,7 @@ bool SharedDatabase::GetInventory(Client *c)
 				inst->SetEvolveActivated(t->activated);
 				inst->SetEvolveEquiped(t->equiped);
 				inst->SetEvolveCurrentAmount(t->current_amount);
-				inst->SetEvolveProgression(t->progression);
+				inst->SetEvolveProgression2();
 				inst->SetEvolveFinalItemID(t->final_item_id);
 			}
 		}
